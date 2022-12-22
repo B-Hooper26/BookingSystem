e612db42089526b6f8a0e605d9d5347b07c038c6
Benjamin Hooper <HOO19000635@btc.ac.uk>
2022-12-22 16:43:48 +0000

    Add project files.

---
+++ BookingSystem/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BookingSystem
+{
+    internal class Customer
+    {
+        //customer properties
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //constructor
+        public Customer (string name, string address, string email, string phonenumber)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            PhoneNumber = phonenumber;
+        }
+        public override string ToString()
+        {
+            //creating conditions so data enter is correct data
+            if (Email.Contains("@") && PhoneNumber.Length == 11)
+            {
+                return $"Customer Details\n\n Name: {Name}\nAddress: {Address}\n Email: {Email}\n Phone Number: {PhoneNumber}";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+    }
+
+}
---
+++ BookingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BookingSystem
+{
+    public class program
+    {
+
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("Ben", "21 tod road", "BH23@123.com", "07411993082");
+
+            Console.WriteLine(c1);
+        }
+    }
+}
